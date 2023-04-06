b5da0e73156305d47d6b828c6ebb267bbf49c2cf
nicosis <nicosis@gmail.com>
2023-04-06 06:23:11 +0000

    add done

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -6,21 +6,16 @@ const ToDoList = () => {
   const [words, setWords] = useState([]);
   console.log(words);
 
-  function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      processInputText();
-    }
-  }
-
   return (
     <div className="container bg-light text-dark">
       <h1>todos</h1>
       <input
         type="text"
         onChange={(e) => setCurrentWord(e.target.value)}
-        // onClick={() => {
-        //   setWords([...words, currentWord]);
-        // }}
+        onKeyDown={(e) => {
+          e.key === "Enter" &&
+            (setWords([...words, currentWord]), setCurrentWord(""));
+        }}
         value={currentWord}
         maxLength={255}
         className="form-control fs-3 mt-2"
@@ -36,27 +31,3 @@ const ToDoList = () => {
 };
 
 export default ToDoList;
-
-/* 
-function App() {
-  const [inputText, setInputText] = useState('');
-
-  function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      processInputText();
-    }
-  }
-
-  function processInputText() {
-    // Aquí puedes procesar el texto ingresado
-    console.log('El texto ingresado es:', inputText);
-    setInputText('');
-  }
-
-  return (
-    <div>
-      <input type="text" value={inputText} onKeyPress={handleKeyPress} onChange={(e) => setInputText(e.target.value)} />
-    </div>
-  );
-}
- */
